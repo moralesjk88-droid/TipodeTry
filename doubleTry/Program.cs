c15a3603b0d2f65c75039b937c0896fbfb8cbe92
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-09 19:41:16 -0600

    doubre try

---
+++ doubleTry/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+  double precio;
+do
+{
+    Console.Clear();
+ Console.WriteLine("Ingrese el precio del producto");
+  
+    if (double.TryParse(Console.ReadLine(), out precio)) 
+    {
+        Console.WriteLine("Precio guardado");
+        Console.ReadKey();
+    }
+    else
+    {
+Console.WriteLine("PRECIO NO VALIDO");
+        Console.ReadKey();
+    }
+        
+}
+while (precio<0);
