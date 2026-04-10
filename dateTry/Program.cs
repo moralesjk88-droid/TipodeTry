1a75e58de3149c3980983d194daf164a577fe3de
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-09 20:07:04 -0600

    Date.try

---
+++ dateTry/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+DateTime fecha;
+bool contar;
+do
+{
+    Console.WriteLine("ingrese la fecha:_ ");
+    contar=DateTime.TryParse(Console.ReadLine(), out fecha);
+    if (!contar)
+    {
+        Console.Write("ERROR DE FECHA");
+    }
+    else
+    {
+        Console.WriteLine("Fecha correcta");
+    }
+
+
+}
+while (!contar);
+
+
+
+//probar con datetime.tryparse
