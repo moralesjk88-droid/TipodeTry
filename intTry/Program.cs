d41d00d08c052ab2431c13686a10027f1c6b77dd
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-09 19:28:44 -0600

    Pruva de int

---
+++ intTry/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese cantidad de libros");
+int libro;
+if (int.TryParse(Console.ReadLine(),out libro))
+{
+    if (libro<0)
+    {
+        Console.WriteLine("No es un numero valido");
+    }
+    else
+    {
+        Console.WriteLine("Cantidad guardada");
+    }
+}
+else
+{
+    Console.WriteLine("Error número no valido");  
+}
+
+
+
+
